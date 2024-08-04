6eaf2d4eea1750ffe23bd1a60eeda4502822ada4
Steadystudy <sgmin206@naver.com>
2024-08-04 22:55:00 +0900

    Feat: Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,35 @@
-import { Button } from 'antd';
+import { Avatar, Button, Flex } from 'antd';
+import Account from 'components/Account';
+import BetCard from 'components/BetCard';
+import BottomBar from 'components/BottomBar';
 import Image from 'next/image';
 
-export default function Home() {
-  return <Button className="bg-slate-200 " />;
+export default function Homepage() {
+  return (
+    <div>
+      <div className="relative w-100vw h-25vh bg-blue200 -z-30 ">
+        <Flex align="center" justify="space-between" className="p-4">
+          <Flex align="center" gap={8}>
+            <Avatar />
+            <span>사용자 이름</span>
+          </Flex>
+          <button>
+            <Image src={'/icons/NotificationOff.svg'} width={20} height={20} alt="notification" />
+          </button>
+        </Flex>
+        <Flex className="absolute bottom-[-60px] left-1/2 -translate-x-1/2">
+          <Account />
+        </Flex>
+      </div>
+      <Flex justify="space-between" align="center" className="mt-20 px-4">
+        <h2 className="text-2xl font-bold">진행중인 내기 (2)</h2>
+        <span>더보기</span>
+      </Flex>
+      <Flex vertical align="center" className=" gap-4 p-4">
+        <BetCard />
+        <BetCard />
+      </Flex>
+      <BottomBar />
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,9 @@ const config: Config = {
   content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
+      height: {
+        '25vh': '25vh',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
