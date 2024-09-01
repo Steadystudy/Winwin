8929f5c80ce80dffb1247096939521f62e07fff7
Steadystudy <sgmin206@naver.com>
2024-09-02 00:20:30 +0900

    Feat: header에 jwt token 추가

--- frontend/src/provider/ApolloProvider.tsx
+++ frontend/src/provider/ApolloProvider.tsx
@@ -1,11 +1,52 @@
 'use client';
 
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from '@apollo/client';
+import { LOCALSTORAGE_TOKEN } from '../constants';
 import { PropsWithChildren } from 'react';
+import { setContext } from '@apollo/client/link/context';
+
+const token = localStorage.getItem(LOCALSTORAGE_TOKEN);
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+});
+export const authTokenVar = makeVar(token);
+export const isLoggedInVar = makeVar(Boolean(token));
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      'x-jwt': authTokenVar() || '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  cache: new InMemoryCache(),
-  uri: 'http://localhost:4000/graphql',
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          isLoggedIn: {
+            read() {
+              return isLoggedInVar();
+            },
+          },
+          token: {
+            read() {
+              return authTokenVar();
+            },
+          },
+        },
+      },
+    },
+  }),
   headers: {
     Authorization: 'Bearer',
   },
